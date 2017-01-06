136e08aed87be0410c668d76b8179fbe44260dbe
JulienHe <hoshi.graphi@gmail.com>
2017-01-06 11:29:59 +0900

    Grid baby

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+'use strict';
+import {TapeMachine} from "./hello";
+
+const tp = new TapeMachine();
+tp.record("Hey! So you are working with boilgner. I'm really happy to see you here. If you want to discuss about improvements of this boilerplate, you can send me an email to : hoshi.graphi@gmail.com");
+tp.play();
